e46248dff3c4046b0d1dd3b89fee7c3c43796896
mosguinz <mos.guinz@gmail.com>
2020-09-13 17:15:42 -0700

    Add ID validation logic

--- src/DrivingExamEvaluator.java
+++ src/DrivingExamEvaluator.java
@@ -11,20 +11,39 @@ import java.util.Scanner;
 
 public class DrivingExamEvaluator {
 
-    final String DEFAULT_ID = "999999999";
-    final int MIN_SCORE = 0;
-    final int MAX_SCORE = 500;
-    final double WRITTEN_SCORE_WEIGHT = 0.74;
-    final double PRACTICAL_SCORE_WEIGHT = 0.26;
-    final double PASS_PERCENTAGE = 0.52;
+    static final int ID_MAX_LENGTH = 9;
+    static final String DEFAULT_ID = "999999999";
+    static final int MIN_SCORE = 0;
+    static final int MAX_SCORE = 500;
+    static final double WRITTEN_SCORE_WEIGHT = 0.74;
+    static final double PRACTICAL_SCORE_WEIGHT = 0.26;
+    static final double PASS_PERCENTAGE = 0.52;
 
     public static void main(String[] args) {
         loop();
     }
 
+    /**
+     * Validate the given student ID.
+     * Prints a message if the given ID is invalid and returns the fallback ID.
+     *
+     * <i>Note: Only checks the length of the ID. Does not check if it is a
+     * valid numerical ID.</i>
+     *
+     * @param id The ID to validate.
+     * @return A valid ID.
+     */
+    private static String validateID(String id) {
+        if (id.length() > ID_MAX_LENGTH) {
+            System.out.printf("%nInvalid ID. Defaulting to: %s.", DEFAULT_ID);
+            return DEFAULT_ID;
+        }
+        return id;
+    }
+
     public static void loop() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your student ID: ");
-        scan.nextLine();
+        String id = validateID(scan.nextLine());
     }
 }
