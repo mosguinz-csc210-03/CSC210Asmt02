65cd8f3d340f0879765685f5e3f07afbdfa643f1
mosguinz <mos.guinz@gmail.com>
2020-09-13 21:24:25 -0700

    Make fields private

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -12,9 +12,9 @@ import java.util.Random;
 
 public class GuessingGame {
 
-    private static int MIN_BOUND = 1;
-    private static int MAX_BOUND = 100;
-    private static int MAX_GUESSES = 10;
+    private static final int MIN_BOUND = 1;
+    private static final int MAX_BOUND = 100;
+    private static final int MAX_GUESSES = 10;
 
     public static void main(String[] args) {
         System.out.println("GUESS MY NUMBER!");
@@ -22,11 +22,5 @@ public class GuessingGame {
         System.out.println("I'll tell you if it is too high or too low for each guess.");
         System.out.printf("See if you can guess it in %d rounds or fewer!%n%n", MAX_GUESSES);
 
-        Scanner scan = new Scanner(System.in);
-        int guess;
-        while (true) {
-            System.out.print(">>> Guess a number: ");
-            guess = scan.nextInt();
-        }
     }
 }
