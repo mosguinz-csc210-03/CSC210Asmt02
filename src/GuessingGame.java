3c905867bbd3ebff9300e879a124863727d4a806
mosguinz <mos.guinz@gmail.com>
2020-09-14 14:58:10 -0700

    Add comments to comply with (3)(b)

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -12,6 +12,7 @@ import java.util.Random;
 
 public class GuessingGame {
 
+    /* Parameters for the game. The bounds are inclusive. */
     private static final int MIN_BOUND = 1;
     private static final int MAX_BOUND = 100;
     private static final int MAX_GUESSES = 10;
@@ -32,35 +33,56 @@ public class GuessingGame {
 
     /**
      * Run the game's execution loop.
+     * Much of the method is self-explanatory, but comments have been added to further
+     * explain its purpose (and to comply with part (3)(b) of the assessment).
      */
     private static void loop() {
+        /* To keep track of the number of guesses. */
         int totalGuesses = 0;
-        int delta;
+
+        /* Generate the target number, inclusive of the bounds. */
         int targetNumber = new Random().nextInt(MAX_BOUND - MIN_BOUND + 1) + MIN_BOUND;
+
+        /* To check how far off the user's guess is from the `targetNumber`. */
+        int delta;
+
+        /* Create a Scanner object. */
         Scanner scan = new Scanner(System.in);
 
         printHeader();
 
         while (true) {
+            /* Increment the number of guesses by one. */
             totalGuesses++;
+
+            /* Scan the user's input and calculate the delta value (difference). */
             System.out.printf(">>> Guess #%d: ", totalGuesses);
             delta = targetNumber - scan.nextInt();
 
+            /* Print the hint messages according to how far the user's guess is. */
             if (delta > 0) {
+                /* If the difference is positive, then the guess is too low. */
                 System.out.println("[x] Too low!");
             } else if (delta < 0) {
+                /* If the difference is negative, then the guess is too high. */
                 System.out.println("[x] Too high!");
             } else {
+                /* If the difference is zero, then the user has guessed correctly; break. */
                 System.out.print("Awesome, you won!");
                 break;
             }
 
+            /*
+             If the user has reached the guess limit and didn't break from else clause
+             above, then they've lost.
+            */
             if (totalGuesses == MAX_GUESSES) {
                 System.out.print("Game over!");
                 break;
             }
         }
 
+        /* Reveal the answer for either cases. */
         System.out.printf(" The number was %d.%n", targetNumber);
     }
 }
