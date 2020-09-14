1c82af589d813d502851f1cb04ca7b9dbb505d97
mosguinz <mos.guinz@gmail.com>
2020-09-13 21:17:02 -0700

    Add welcome message for game

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -7,8 +7,26 @@
  * Semester: Fall 2020
  */
 
-public class GuessingGame {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.Random;
 
+public class GuessingGame {
+
+    private static int MIN_BOUND = 1;
+    private static int MAX_BOUND = 100;
+    private static int MAX_GUESSES = 10;
+
+    public static void main(String[] args) {
+        System.out.println("GUESS MY NUMBER!");
+        System.out.printf("I've selected a number between %d to %d.%n", MIN_BOUND, MAX_BOUND);
+        System.out.println("I'll tell you if it is too high or too low for each guess.");
+        System.out.printf("See if you can guess it in %d rounds or fewer!%n%n", MAX_GUESSES);
+
+        Scanner scan = new Scanner(System.in);
+        int guess;
+        while (true) {
+            System.out.print(">>> Guess a number: ");
+            guess = scan.nextInt();
+        }
     }
 }
