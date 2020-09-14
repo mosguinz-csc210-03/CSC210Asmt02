119ee4cfb098d30b43c0e0f0471c533b9d9b3098
mosguinz <mos.guinz@gmail.com>
2020-09-13 21:01:47 -0700

    Update file headers

---
+++ src/GuessingGame.java
@@ -0,0 +1,14 @@
+/*
+ * Assignment 02
+ * Description: [Part 2 of 2] Guessing game
+ * Name: Kullathon "Mos" Sitthisarnwattanachai
+ * ID: 921425216
+ * Class: CSC 210-03
+ * Semester: Fall 2020
+ */
+
+public class GuessingGame {
+    public static void main(String[] args) {
+
+    }
+}
