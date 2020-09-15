536901822edc35d9aaf3db5481848d3f483a21a4
mosguinz <mos.guinz@gmail.com>
2020-09-14 19:03:40 -0700

    Add more comments to comply with (3)(b)

--- src/DrivingExamEvaluator.java
+++ src/DrivingExamEvaluator.java
@@ -11,12 +11,13 @@ import java.util.Scanner;
 
 public class DrivingExamEvaluator {
 
-    static final int ID_MAX_LENGTH = 9;
+    /* Parameters for the exam evaluator */
     static final String DEFAULT_ID = "999999999";
+    static final int ID_MAX_LENGTH = 9;
     static final int MIN_SCORE = 0;
     static final int MAX_SCORE = 500;
 
-    // In percentage
+    /* Score weights and passing percentage. */
     static final double WRITTEN_SCORE_WEIGHT = 74;
     static final double PRACTICAL_SCORE_WEIGHT = 26;
     static final double PASS_PERCENTAGE = 52;
@@ -36,6 +37,7 @@ public class DrivingExamEvaluator {
      * @return A valid ID.
      */
     private static String validateID(String id) {
+        /* Return the DEFAULT_ID and notify the user. */
         if (id.length() > ID_MAX_LENGTH) {
             System.out.printf("Invalid ID. Defaulting to: %s.%n", DEFAULT_ID);
             return DEFAULT_ID;
@@ -52,8 +54,10 @@ public class DrivingExamEvaluator {
      * @return A valid score.
      */
     private static double validateScore(double score) {
+        /* Override scores with their defaults if they are out of bounds. */
         if (score > MAX_SCORE) score = MAX_SCORE;
         else if (score < MIN_SCORE) score = MIN_SCORE;
+        /* Skip the print and return the original score if it is valid. */
         else return score;
 
         System.out.printf("Invalid score. Defaulting to: %.0f.%n", score);
@@ -67,18 +71,22 @@ public class DrivingExamEvaluator {
      * @param practicalScore The (validated) practical score.
      */
     private static void calculateScore(double writtenScore, double practicalScore) {
+        /* Calculate the weighted scores and its total. */
         double weightedWritten = WRITTEN_SCORE_WEIGHT * (writtenScore / MAX_SCORE);
         double weightedPractical = PRACTICAL_SCORE_WEIGHT * (practicalScore / MAX_SCORE);
         double totalWeighted = weightedWritten + weightedPractical;
 
+        /* Display score summary. */
         String scoreMessage = "%nYour %s exam, weighted %.0f%% of your total score, is: %.2f%%";
         System.out.printf(scoreMessage, "written", WRITTEN_SCORE_WEIGHT, weightedWritten);
         System.out.printf(scoreMessage, "practical", PRACTICAL_SCORE_WEIGHT, weightedPractical);
         System.out.printf("%nYour total score is: %.2f%%%n", totalWeighted);
 
+        /* Print pass/fail message. */
         if (totalWeighted >= PASS_PERCENTAGE) System.out.print("Congratulations, you passed!");
         else System.out.print("Unfortunately, you failed!");
 
+        /* Print passing requirement, regardless of condition. */
         System.out.printf(" A passing grade requires you to score %.0f%% or above.%n", PASS_PERCENTAGE);
     }
 
@@ -103,7 +111,13 @@ public class DrivingExamEvaluator {
      * Run the execution loop.
      */
     public static void loop() {
+        /* Create a Scanner object. */
         Scanner scan = new Scanner(System.in);
+
+        /*
+         Print prompts, scan the input, and send them to
+         their respective validation methods.
+        */
         System.out.print("Enter your student ID: ");
         String studentID = validateID(scan.nextLine());
         System.out.print("Enter your written exam score: ");
@@ -111,6 +125,7 @@ public class DrivingExamEvaluator {
         System.out.print("Enter your practical exam score: ");
         double practicalScore = validateScore(scan.nextDouble());
 
+        /* Send the validated ID and scores to be calculated and displayed. */
         displayResults(studentID, writtenScore, practicalScore);
     }
 }
